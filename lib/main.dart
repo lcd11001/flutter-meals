3debfe800f88c8b92e846ea6bd7b300baafb1b78
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-04-12 14:08:49 +0700

    - testing

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
       theme: lightThemeData,
       darkTheme: darkThemeData,
       home: const CategoriesScreen(),
